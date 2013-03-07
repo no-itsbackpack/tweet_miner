0c6ca7ee9a5aa43910b9cddc365935fb920e3ea0
Elom Gomez <egomez@enova.com>
2013-03-07 15:35:35 -0600

    Setup the database for tweets and miner

---
+++ app/models/miner.rb
@@ -0,0 +1,3 @@
+class Miner < ActiveRecord::Base
+  attr_accessible :link
+end
---
+++ db/migrate/20130307210943_setup_database.rb
@@ -0,0 +1,19 @@
+class SetupDatabase < ActiveRecord::Migration
+  def up
+    create_table :miners do |t|
+      t.string :link
+      t.timestamps
+    end
+
+    create_table :tweets do |t|
+      t.references :miner
+      t.text :description
+      t.string :author
+      t.timestamps
+    end
+  end
+
+  def down
+    raise IrreversibleMigration
+  end
+end
