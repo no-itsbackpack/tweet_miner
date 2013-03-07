0d742261354b793d88c78bbff8bf80682fa626fd
Elom Gomez <egomez@enova.com>
2013-03-07 15:02:04 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TweetMiner::Application.config.secret_token = '032508570efa9ed062c6817c15221e3711901df3ae85670e26e50222315d95bea6a28b1858205fc73c036072dcc97a5c8003e1569e2ce8e8a944e7d3a51696b5'
